6012bba1a069957527ee1c00f4116eafba46226f
sbhagerty <sbhagerty@gmail.com>
2022-07-25 04:18:22 +0000

    test

--- git_setup.R
+++ git_setup.R
@@ -2,7 +2,7 @@ library(usethis)
 library(gitcreds)
 
 
-usethis::use_git_config(user.name = “Jane Doe”, user.email = “jane@example.org”)
+usethis::use_git_config(user.name = "sbhagerty", user.email = "sbhagerty@gmail.com")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
 
